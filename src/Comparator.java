ed762da1a95f2f5af894fcc4c8e563413c62ef8e
eldonwang <731365710@qq.com>
2018-12-07 00:59:31 +0800

    左神初级班第三课

---
+++ src/Comparator.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Comparator {
+    public static class Student {
+        String name;
+        int age;
+        int id;
+
+        public Student(String name, int age, int id) {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+        }
+    }
+
+    public static class IdAscendingComparator implements java.util.Comparator<Student> {
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.id - o1.id;
+        }
+    }
+
+    public static void printStudents(Student[] students) {
+        for (Student student : students) {
+            System.out.println("Name : " + student.name + ", Age : " + student.age + ", Id : " + student.id);
+        }
+        System.out.println("===========================");
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("A", 1, 23);
+        Student student2 = new Student("B", 2, 21);
+        Student student3 = new Student("C", 3, 22);
+
+        Student[] students = new Student[]{student3, student2, student1};
+        printStudents(students);
+
+        Arrays.sort(students, new IdAscendingComparator());
+        printStudents(students);
+    }
+}
+//开大幅度发
+
